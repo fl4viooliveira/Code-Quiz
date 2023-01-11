5c3209d5c67e25aa24ddd5038e10d7b08a340e4f
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-01-11 11:29:12 +0000

    Add start button function
    The function start the timer calling globalTimer function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,22 @@
 console.log("test by logic file")
-
+// Get Elements
+var startBtn = document.getElementById("start")
 var timerSpan = document.getElementById("time")
 
+// Timer function
 var timeLeft = 75;
 
 function globalTimer(){
   
-  var timerInterval = setInterval(function(){
+  setInterval(function(){
     timeLeft--;
     timerSpan.textContent = timeLeft;
   }, 1000);
 
 }
 
-globalTimer()
+// Start button function
+
+startBtn.addEventListener("click", function(){
+  globalTimer()
+})
