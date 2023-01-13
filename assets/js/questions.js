2c56e397bfc055284d4350b16056e07203f3a680
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-01-13 15:43:03 +0000

    Create a test questions
    Variable question is a array of objects for each question

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,31 @@
-console.log("test by questions file")
+// Array with one Object for each question.
+
+var questions = [
+  {
+    question: "How much is 1 + 1?",
+    answers: {
+      a: "11",
+      b: "1",
+      c: "2",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "How much is 1 + 2?",
+    answers: {
+      a: "12",
+      b: "3",
+      c: "2",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "How much is 2 + 2?",
+    answers: {
+      a: "4",
+      b: "22",
+      c: "44",
+    },
+    correctAnswer: "a"
+  },
+];
