d692b311f77b6ccd51d7dd722d13cff0babf16f6
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-01-13 15:20:17 +0000

    Refactor the initial code
    Create variable to elements

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,29 @@
-console.log("test by logic file")
 // Get Elements
-var startBtn = document.getElementById("start")
-var timerSpan = document.getElementById("time")
+var timerSpan = document.querySelector(".timer");
+var container = document.querySelector(".wrapper");
+var startBlock = document.querySelector("#start-screen");
+var startBtn = document.querySelector("#start");
+
+var quizContainer = document.querySelector("#questions");
+var questionTitle = document.querySelector("#question-title");
+var questionsChoices = document.querySelector("#choices");
+
+var endBlock = document.querySelector("#end-screen")
+
+var feedback = document.querySelector("#feedback")
 
 // Timer function
 var timeLeft = 75;
 
-function globalTimer(){
-  
-  setInterval(function(){
+function globalTimer() {
+  setInterval(function () {
     timeLeft--;
     timerSpan.textContent = timeLeft;
   }, 1000);
-
 }
 
 // Start button function
 
-startBtn.addEventListener("click", function(){
-  globalTimer()
-})
+startBtn.addEventListener("click", function () {
+  globalTimer();
+});
